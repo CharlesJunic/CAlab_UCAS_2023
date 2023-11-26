116581c0e0109ee8b4fc81349a018ab804b1d51f
CharlesJunic <bidingjun21@mails.ucas.ac.cn>
2023-11-26 21:40:53 +0800

    exp16

---
+++ myCPU/MEM_stage.v
@@ -0,0 +1,141 @@
+module MEM_stage(
+    input          clk,
+    input          reset,
+    input          WB_allowin,
+    output         MEM_allowin,
+    input          EXE_to_MEM_valid,
+    input  [213:0] EXE_to_MEM_bus,
+    output         MEM_to_WB_valid,
+    output [206:0] MEM_to_WB_bus,
+    input  [ 31:0] data_sram_rdata,
+    input          data_sram_data_ok,
+    output         out_MEM_valid,
+    input          exec_flush
+);
+
+wire [31:0] MEM_pc;
+wire [31:0] MEM_mem_result;
+wire [31:0] MEM_alu_result;
+wire [31:0] MEM_final_result;
+wire [31:0] MEM_ld_result;
+wire [ 4:0] MEM_dest;
+wire [ 1:0] MEM_vaddr;
+wire MEM_res_from_mem;
+wire MEM_gr_we;
+wire MEM_mem_we;
+wire MEM_op_b;
+wire MEM_op_h;
+wire MEM_op_unsigned_ld;
+wire [31:0] MEM_rj_value;
+wire [31:0] MEM_rkd_value;
+wire MEM_inst_csrrd;
+wire MEM_inst_csrwr;
+wire MEM_inst_csrxchg;
+wire MEM_inst_syscall;
+wire MEM_inst_ertn;
+wire [14:0] MEM_ex_code;
+wire [13:0] MEM_csr_num;
+wire MEM_ex_adef;
+wire MEM_ex_ine;
+wire MEM_ex_ale;
+wire MEM_inst_brk;
+wire MEM_inst_rdcntid;
+wire MEM_inst_rdcntvl_w;
+wire MEM_inst_rdcntvh_w;
+wire [31:0] MEM_ex_baddr;
+
+wire MEM_ex;
+assign MEM_ex = MEM_ex_adef |
+               MEM_ex_ale  |
+               MEM_ex_ine |
+               MEM_inst_syscall |
+               MEM_inst_brk |
+               MEM_inst_ertn;
+
+reg MEM_valid;
+wire MEM_ready_go;
+reg [213:0] EXE_to_MEM_bus_rf;
+
+assign MEM_ready_go = (MEM_res_from_mem | MEM_mem_we) ? (data_sram_data_ok | exec_flush | MEM_ex) : 1'b1;
+assign MEM_allowin = !MEM_valid || MEM_ready_go && WB_allowin;
+assign out_MEM_valid = MEM_valid;
+
+always @(posedge clk) begin
+    if(reset)
+        MEM_valid <= 1'b0;
+    else if(exec_flush)
+        MEM_valid <= 1'b0;
+    else if(MEM_allowin)
+        MEM_valid <= EXE_to_MEM_valid;
+end
+
+always @(posedge clk) begin
+    if(MEM_allowin && EXE_to_MEM_valid)
+        EXE_to_MEM_bus_rf <= EXE_to_MEM_bus;
+end
+
+assign {
+    MEM_mem_we,
+    MEM_ex_adef,
+    MEM_ex_ine,
+    MEM_ex_ale,
+    MEM_ex_baddr[31:0],
+    MEM_inst_brk,
+    MEM_inst_rdcntid,
+    MEM_inst_rdcntvl_w,
+    MEM_inst_rdcntvh_w,
+    MEM_ex_code[14:0],    
+    MEM_rj_value[31:0],     
+    MEM_rkd_value[31:0],    
+    MEM_inst_syscall,       
+    MEM_inst_ertn,           
+    MEM_inst_csrrd,        
+    MEM_inst_csrwr,       
+    MEM_inst_csrxchg,       
+    MEM_csr_num[13:0],     
+    MEM_vaddr,
+    MEM_op_unsigned_ld,
+    MEM_op_b,
+    MEM_op_h,
+    MEM_pc[31:0],
+    MEM_alu_result[31:0],
+    MEM_res_from_mem,
+    MEM_gr_we,
+    MEM_dest[4:0] } = EXE_to_MEM_bus_rf[213:0];
+
+assign MEM_mem_result   = data_sram_rdata;
+assign MEM_ld_result = {32{MEM_op_b & MEM_vaddr == 2'b00}} & {{24{~MEM_op_unsigned_ld & MEM_mem_result[ 7]}},MEM_mem_result[ 7: 0]} |
+                       {32{MEM_op_b & MEM_vaddr == 2'b01}} & {{24{~MEM_op_unsigned_ld & MEM_mem_result[15]}},MEM_mem_result[15: 8]} |
+                       {32{MEM_op_b & MEM_vaddr == 2'b10}} & {{24{~MEM_op_unsigned_ld & MEM_mem_result[23]}},MEM_mem_result[23:16]} |
+                       {32{MEM_op_b & MEM_vaddr == 2'b11}} & {{24{~MEM_op_unsigned_ld & MEM_mem_result[31]}},MEM_mem_result[31:24]} |
+                       {32{MEM_op_h & MEM_vaddr == 2'b00}} & {{16{~MEM_op_unsigned_ld & MEM_mem_result[15]}},MEM_mem_result[15: 0]} |
+                       {32{MEM_op_h & MEM_vaddr == 2'b10}} & {{16{~MEM_op_unsigned_ld & MEM_mem_result[31]}},MEM_mem_result[31:16]} |
+                       {32{~MEM_op_b & ~MEM_op_h}} & MEM_mem_result[31:0];
+assign MEM_final_result = MEM_res_from_mem ? MEM_ld_result : MEM_alu_result;
+assign MEM_to_WB_valid = MEM_valid && MEM_ready_go;
+
+assign MEM_to_WB_bus[206:0] = {
+    MEM_ex_adef,            //206
+    MEM_ex_ine,             //205
+    MEM_ex_ale,             //204
+    MEM_ex_baddr[31:0],     //203:172
+    MEM_inst_brk,           //171
+    MEM_inst_rdcntid,       //170
+    MEM_inst_rdcntvl_w,     //169
+    MEM_inst_rdcntvh_w,     //168
+    MEM_ex_code[14:0],      //167:153
+    MEM_rj_value[31:0],     //152:121
+    MEM_rkd_value[31:0],    //120:89
+    MEM_inst_syscall,       //88:88
+    MEM_inst_ertn,          //87:87
+    MEM_inst_csrrd,         //86:86
+    MEM_inst_csrwr,         //85:85
+    MEM_inst_csrxchg,       //84:84
+    MEM_csr_num[13:0],      //83:70
+    MEM_pc[31:0],           //69:38         
+    MEM_gr_we,              //37:37
+    MEM_dest[4:0],          //36:32
+    MEM_final_result[31:0]  //31:0
+};
+
+endmodule
--- myCPU/mycpu_top.v
+++ myCPU/mycpu_top.v
@@ -1,109 +1,154 @@
 module mycpu_top(
-    input  wire        clk,
-    input  wire        resetn,
-    // inst sram interface
-    output wire        inst_sram_en,
-    output wire [ 3:0] inst_sram_we,
-    output wire [31:0] inst_sram_addr,
-    output wire [31:0] inst_sram_wdata,
-    input  wire [31:0] inst_sram_rdata,
-    // data sram interface
-    output wire        data_sram_en,
-    output wire [ 3:0] data_sram_we,
-    output wire [31:0] data_sram_addr,
-    output wire [31:0] data_sram_wdata,
-    input  wire [31:0] data_sram_rdata,
-    // trace debug interface
-    output wire [31:0] debug_wb_pc,
-    output wire [ 3:0] debug_wb_rf_we,
-    output wire [ 4:0] debug_wb_rf_wnum,
-    output wire [31:0] debug_wb_rf_wdata
+    input         aclk,         //axi时钟
+    input         aresetn,      //axi复位，低电平有效
+    output [ 3:0] arid,         //读请求的id号，取指0，取数1
+    output [31:0] araddr,       //读请求地址
+    output [ 7:0] arlen,        //读请求传输长度，固定为0
+    output [ 2:0] arsize,       //读请求传输大小
+    output [ 1:0] arburst,      //读请求传输类型
+    output [ 1:0] arlock,       //读请求原子锁
+    output [ 3:0] arcache,      //读请求cache属性
+    output [ 2:0] arprot,       //读请求保护属性
+    output        arvalid,      //读请求握手信号，地址有效
+    input         arready,      //读请求握手信号，slave端准备好接收地址传输
+    input  [ 3:0] rid,          //读请求的id号，同一请求的rid和arid一致，取指0，取数1
+    input  [31:0] rdata,        //读请求读回数据
+    input  [ 1:0] rresp,        //读请求，本次是否成功完成，可忽略
+    input         rlast,        //读请求，数据有效，可忽略
+    input         rvalid,       //读请求握手信号，数据有效
+    input         rready,       //读请求握手信号，master端准备好接受数据传输
+    output [ 3:0] awid,         //写请求的id号，固定为1
+    output [31:0] awaddr,       //写请求的地址
+    output [ 7:0] awlen,        //写请求，请求传输长度，固定为0
+    output [ 2:0] awsize,       //写请求，请求传输大小
+    output [ 1:0] awburst,      //写请求，传输类型，固定为0b01
+    output [ 1:0] awlock,       //写请求，原子锁，固定为0
+    output [ 3:0] awcache,      //写请求，cache属性，固定为0
+    output [ 2:0] awprot,       //写请求，保护属性，固定为0
+    output        awvalid,      //写请求握手信号，地址有效
+    input         awready,      //写请求握手信号，slave端准备好接受地址传输
+    output [ 3:0] wid,          //写请求的id号，固定为1
+    output [31:0] wdata,        //写请求数据
+    output [ 3:0] wstrb,        //写请求，字节选通位
+    output        wlast,        //写请求，本次写请求的最后一拍数据的指示信号，固定为1
+    output        wvalid,       //写请求握手信号，数据有效
+    output        wready,       //写请求握手信号，slave端准备好接受数据传输
+    input  [ 3:0] bid,          //写请求的id号，同一请求的bid，wid，awid一致，可忽略
+    input  [ 1:0] bresp,        //写请求，本次写请求是否成功完成，可忽略
+    input         bvalid,       //写请求握手信号，写请求响应有效
+    output        bready,       //写请求握手信号，master端准备好接受写响应
+    output [31:0] debug_wb_pc,
+    output [ 3:0] debug_wb_rf_we,
+    output [ 4:0] debug_wb_rf_wnum,
+    output [31:0] debug_wb_rf_wdata
 );
-    reg         reset;
-    always @(posedge clk) reset <= ~resetn;
 
-    wire            if_id_valid;
-    wire            id_allowin;
-    wire            id_exe_valid;
-    wire            exe_allowin;
-    wire            exe_mem_valid;
-    wire            mem_allowin;
-    wire            mem_wb_valid;
-    wire            wb_allowin;
+/******************** DECLARATION ********************/
+    wire        cpu_inst_req;
+    wire        cpu_inst_wr;
+    wire [ 1:0] cpu_inst_size;
+    wire [31:0] cpu_inst_addr;
+    wire [ 3:0] cpu_inst_wstrb;
+    wire [31:0] cpu_inst_wdata;
+    wire        cpu_inst_addr_ok;
+    wire        cpu_inst_data_ok;
+    wire [31:0] cpu_inst_rdata;
+    wire        cpu_data_req;
+    wire        cpu_data_wr;
+    wire [ 1:0] cpu_data_size;
+    wire [31:0] cpu_data_addr;
+    wire [ 3:0] cpu_data_wstrb;
+    wire [31:0] cpu_data_wdata;
+    wire        cpu_data_addr_ok;
+    wire        cpu_data_data_ok;
+    wire [31:0] cpu_data_rdata;
 
-    wire    [ 63:0] if_id_bus;
-    wire    [ 32:0] id_if_bus;
-    wire    [179:0] id_exe_bus;
-    wire    [102:0] exe_mem_bus;
-    wire    [ 38:0] exe_wr_bus;
-    wire    [101:0] mem_wb_bus;
-    wire    [ 37:0] mem_wr_bus;
-    wire    [ 37:0] wb_id_bus;
+/******************** CPU CORE ********************/
+    mycpu_core u_mycpu_core(
+        .clk(aclk),
+        .resetn(aresetn),
+        .inst_sram_req(cpu_inst_req),
+        .inst_sram_wr(cpu_inst_wr),
+        .inst_sram_size(cpu_inst_size),
+        .inst_sram_addr(cpu_inst_addr),
+        .inst_sram_wstrb(cpu_inst_wstrb),
+        .inst_sram_wdata(cpu_inst_wdata),
+        .inst_sram_addr_ok(cpu_inst_addr_ok),
+        .inst_sram_data_ok(cpu_inst_data_ok),
+        .inst_sram_rdata(cpu_inst_rdata),
+        .data_sram_req(cpu_data_req),
+        .data_sram_wr(cpu_data_wr),
+        .data_sram_size(cpu_data_size),
+        .data_sram_addr(cpu_data_addr),
+        .data_sram_wstrb(cpu_data_wstrb),
+        .data_sram_wdata(cpu_data_wdata),
+        .data_sram_addr_ok(cpu_data_addr_ok),
+        .data_sram_data_ok(cpu_data_data_ok),
+        .data_sram_rdata(cpu_data_rdata),
+        .debug_wb_pc(debug_wb_pc),
+        .debug_wb_rf_we(debug_wb_rf_we),
+        .debug_wb_rf_wnum(debug_wb_rf_wnum),
+        .debug_wb_rf_wdata(debug_wb_rf_wdata)
+    );
 
-    IF my_if (
-        .clk                (clk),
-        .reset              (reset),
-        .id_allowin         (id_allowin),
-        .if_id_valid        (if_id_valid),
-        .if_id_bus          (if_id_bus),
-        .id_if_bus          (id_if_bus),
-        .inst_sram_en       (inst_sram_en),
-        .inst_sram_we       (inst_sram_we),
-        .inst_sram_addr     (inst_sram_addr),
-        .inst_sram_rdata    (inst_sram_rdata),
-        .inst_sram_wdata    (inst_sram_wdata)
+/******************** AXI BRIDGE ********************/
+    axi_bridge u_axi_bridge(
+        .clk(aclk),
+        .resetn(aresetn),
+        .inst_sram_req(cpu_inst_req),
+        .inst_sram_wr(cpu_inst_wr),
+        .inst_sram_size(cpu_inst_size),
+        .inst_sram_addr(cpu_inst_addr),
+        .inst_sram_wstrb(cpu_inst_wstrb),
+        .inst_sram_wdata(cpu_inst_wdata),
+        .inst_sram_addr_ok(cpu_inst_addr_ok),
+        .inst_sram_data_ok(cpu_inst_data_ok),
+        .inst_sram_rdata(cpu_inst_rdata),
+        .data_sram_req(cpu_data_req),
+        .data_sram_wr(cpu_data_wr),
+        .data_sram_size(cpu_data_size),
+        .data_sram_addr(cpu_data_addr),
+        .data_sram_wstrb(cpu_data_wstrb),
+        .data_sram_wdata(cpu_data_wdata),
+        .data_sram_addr_ok(cpu_data_addr_ok),
+        .data_sram_data_ok(cpu_data_data_ok),
+        .data_sram_rdata(cpu_data_rdata),
+        .arid(arid),
+        .araddr(araddr),
+        .arlen(arlen),
+        .arsize(arsize),
+        .arburst(arburst),
+        .arlock(arlock),
+        .arcache(arcache),
+        .arprot(arprot),
+        .arvalid(arvalid),
+        .arready(arready),
+        .rid(rid),
+        .rdata(rdata),
+        .rresp(rresp),
+        .rlast(rlast),
+        .rvalid(rvalid),
+        .rready(rready),
+        .awid(awid),
+        .awaddr(awaddr),
+        .awlen(awlen),
+        .awsize(awsize),
+        .awburst(awburst),
+        .awlock(awlock),
+        .awcache(awcache),
+        .awprot(awprot),
+        .awvalid(awvalid),
+        .awready(awready),
+        .wid(wid),
+        .wdata(wdata),
+        .wstrb(wstrb),
+        .wlast(wlast),
+        .wvalid(wvalid),
+        .wready(wready),
+        .bid(bid),
+        .bresp(bresp),
+        .bvalid(bvalid),
+        .bready(bready)
     );
-    ID my_id (
-        .clk                (clk),
-        .reset              (reset),
-        .if_id_valid        (if_id_valid),
-        .id_allowin         (id_allowin),
-        .if_id_bus          (if_id_bus),
-        .id_if_bus          (id_if_bus),
-        .exe_allowin        (exe_allowin),
-        .id_exe_valid       (id_exe_valid),
-        .id_exe_bus         (id_exe_bus),
-        .wb_id_bus          (wb_id_bus),
-        .exe_wr_bus         (exe_wr_bus),
-        .mem_wr_bus         (mem_wr_bus)
-    );
-    EXE my_exe (
-        .clk                (clk),
-        .reset              (reset),
-        .exe_allowin        (exe_allowin),
-        .id_exe_valid       (id_exe_valid),
-        .id_exe_bus         (id_exe_bus),
-        .exe_mem_valid      (exe_mem_valid),
-        .mem_allowin        (mem_allowin),
-        .exe_mem_bus        (exe_mem_bus),
-        .data_sram_en       (data_sram_en),
-        .data_sram_we       (data_sram_we),
-        .data_sram_addr     (data_sram_addr),
-        .data_sram_wdata    (data_sram_wdata),
-        .exe_wr_bus         (exe_wr_bus)
-    );
-    MEM my_mem (
-        .clk                (clk),
-        .reset              (reset),
-        .mem_allowin        (mem_allowin),
-        .exe_mem_valid      (exe_mem_valid),
-        .exe_mem_bus        (exe_mem_bus),
-        .mem_wb_valid       (mem_wb_valid),
-        .wb_allowin         (wb_allowin),
-        .mem_wb_bus         (mem_wb_bus),
-        .data_sram_rdata    (data_sram_rdata),
-        .mem_wr_bus         (mem_wr_bus)
-    );
-    WB my_wb (
-        .clk                (clk),
-        .reset              (reset),
-        .wb_allowin         (wb_allowin),
-        .mem_wb_valid       (mem_wb_valid),
-        .mem_wb_bus         (mem_wb_bus),
-        .wb_id_bus          (wb_id_bus),
-        .debug_wb_pc        (debug_wb_pc),
-        .debug_wb_rf_we     (debug_wb_rf_we),
-        .debug_wb_rf_wnum   (debug_wb_rf_wnum),
-        .debug_wb_rf_wdata  (debug_wb_rf_wdata)
-    );
+
 endmodule
